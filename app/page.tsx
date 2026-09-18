dd2a8799c2cf0d6788369edbda857487cabd2957
ULUDEX <uludex.tr@gmail.com>
2026-09-18 15:36:33 +0300

    Use rewrite for root storefront compatibility

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,5 @@
-import fs from "node:fs/promises";
-import path from "node:path";
+import { redirect } from "next/navigation";
 
-export const dynamic = "force-static";
-
-export default async function Home() {
-  const filePath = path.join(process.cwd(), "public", "site", "index.html");
-  const html = await fs.readFile(filePath, "utf8");
-
-  return (
-    <iframe
-      srcDoc={html}
-      title="ElTurco SMM"
-      style={{ width: "100%", minHeight: "100vh", border: 0, display: "block" }}
-    />
-  );
+export default function Home() {
+  redirect("/site/");
 }
